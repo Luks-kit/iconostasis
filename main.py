dddfdae50e67e5aa97116b9b28ee1c96be165a10
Lucas Tesfaye <lushdudeaimboot@gmail.com>
2026-02-14 13:24:00 -0600

    connect a bit safer

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ connect_args = {}
 if DATABASE_URL.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+engine = create_engine(DATABASE_URL, connect_args=connect_args, pool_pre_ping=True)
 Base.metadata.create_all(bind=engine)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
